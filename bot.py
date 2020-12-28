0758a311e3cd694d90e186d91f668452fdea292d
Daniel <danielalvesbarreleiro@gmail.com>
2020-12-28 18:59:00 +0000

    Code Cleanup
    Divided everything into functions.
    Some other cleanups.

--- bot.py
+++ bot.py
@@ -7,17 +7,6 @@ SL = 'https://steamladder.com/api/v1'
 # Steam WEB API Base URL
 SW = 'https://api.steampowered.com'
 
-
-def getSteamID():
-    print('Input Steam Custom URL')
-    steamid = input()
-    # Resolve Vanity URL
-    rvurl = '{}/ISteamUser/ResolveVanityURL/v1/?key={}&vanityurl={}'.format(SW, SWK, steamid)
-    rvurl_response = requests.get(rvurl)
-    rvurl_data = rvurl_response.json()
-    steamid = rvurl_data['response']['steamid']
-    return steamid
-
 def getSteamStatus():
     status_url = '{}/ICSGOServers_730/GetGameServersStatus/v1/?key={}'.format(SW, SWK)
     status_response = requests.get(status_url)
@@ -31,51 +20,77 @@ def getSteamStatus():
     if (steam_store != 'normal'):
         steam_store =  '{} | {}'.format(steam_store, steam_store_b)
 
-    print('Steam Store:', steam_store)
-    print('Sessions Logon:', session_logon)
-    print('Player Inventories:', player_inv)
-    print('CS:GO Match Making Scheduler:', match_scheduler)
+    print('Steam Store:', steam_store.capitalize())
+    print('Sessions Logon:', session_logon.capitalize())
+    print('Player Inventories:', player_inv.capitalize())
+    print('CS:GO Match Making Scheduler:', match_scheduler.capitalize())
 
-def getSteamProfileData():
-    steamid = getSteamID()
-    # URL0 - GET PLAYER SUMMARIES
-    url0 = '{}/ISteamUser/GetPlayerSummaries/v2/?key={}&steamids={}'.format(SW, SWK, steamid)
-    url0_response = requests.get(url0)
-    data0 = url0_response.json()
+def getSteamID():
+    print('Input Steam Custom URL')
+    steamid = input()
+    # Resolve Vanity URL
+    rvurl = '{}/ISteamUser/ResolveVanityURL/v1/?key={}&vanityurl={}'.format(SW, SWK, steamid)
+    rvurl_response = requests.get(rvurl)
+    rvurl_data = rvurl_response.json()
+    steamid = rvurl_data['response']['steamid']
 
-    # URL1 - GET PLAYER BANS
-    url1 = '{}/ISteamUser/GetPlayerBans/v1/?key={}&steamids={}'.format(SW, SWK, steamid)
-    url1_response = requests.get(url1)
-    data1 = url1_response.json()
+    getSteamProfileData(steamid)
 
-    name = data0['response']['players'][0]['personaname']
-    avatar = data0['response']['players'][0]['avatarfull']
-    country = data0['response']['players'][0]['loccountrycode']
+def getSteamProfileData(steamid):
+    # GET PLAYER SUMMARIES
+    profile_url = '{}/ISteamUser/GetPlayerSummaries/v2/?key={}&steamids={}'.format(SW, SWK, steamid)
+    profile_response = requests.get(profile_url)
+    profile_data = profile_response.json()
+    
+    name = profile_data['response']['players'][0]['personaname']
+    avatar = profile_data['response']['players'][0]['avatarfull']
+    country = profile_data['response']['players'][0]['loccountrycode']
 
-    vac_ban = str(data1['players'][0]['VACBanned'])
-    trade_ban = str(data1['players'][0]['EconomyBan'])
-    community_ban = str(data1['players'][0]['CommunityBanned'])
+    print('Name:', name, '\nAvatar:', avatar, '\nCountry:', country,'\n')
+    getOwnedGames(steamid)
+
+def getPlayerBans(steamid):
+    # GET PLAYER BANS
+    bans_url = '{}/ISteamUser/GetPlayerBans/v1/?key={}&steamids={}'.format(SW, SWK, steamid)
+    bans_response = requests.get(bans_url)
+    bans_data = bans_response.json()
+
+    vac_ban = str(bans_data['players'][0]['VACBanned'])
+    trade_ban = str(bans_data['players'][0]['EconomyBan'])
+    community_ban = str(bans_data['players'][0]['CommunityBanned'])
 
     if vac_ban == 'false' or vac_ban == 'False':
         vac_ban = 'None'
     if vac_ban == 'true' or vac_ban == 'True':
       vac_ban = 'Banned'
 
-    if trade_ban == 'none' or trade_ban == 'None':
-      trade_ban = 'None'
-    if trade_ban == 'banned' or trade_ban == 'Banned':
-      trade_ban = 'Banned'
+    trade_ban = trade_ban.capitalize()
 
     if community_ban == 'false' or community_ban == 'False':
       community_ban = 'None'
     if community_ban == 'true' or community_ban == 'True':
         community_ban = 'Banned'
 
-    print('Name:', name)
-    print('Avatar:', avatar)
-    print('Country', country)
-    print('VAC Ban:', vac_ban)
-    print('Trade Ban:', trade_ban)
-    print('Community Ban:', community_ban)
+    print('--- BANS ---')
+    print('VAC Ban:', vac_ban, '\nTrade Ban:', trade_ban, '\nCommunity Ban:', community_ban, '\n')
 
-getSteamProfileData()
+def getOwnedGames(steamid):
+    # URL2 - GET PLAYER OWNED GAMES
+    ownedgames_url = '{}/IPlayerService/GetOwnedGames/v1/?key={}&steamid={}&include_played_free_games=1'.format(SW, SWK, steamid)
+    ownedgames_response = requests.get(ownedgames_url)
+    owned_games = ownedgames_response.json()
+
+    total_games = owned_games['response']['game_count']
+
+    print('Total Games:', total_games, '\n')
+    getPlayerBans(steamid)
+
+def Menu():
+  print('What do you want to do? \n 1 - Steam Status \n 2 - Player Data')
+  option = input()
+  if option == '1':
+    getSteamStatus()
+  if option == '2':
+    getSteamID()
+
+Menu()

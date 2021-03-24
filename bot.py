93ab3cbdc429c573c5b0fb4edf1bc5205a84159d
Daniel <danielalvesbarreleiro@gmail.com>
2021-03-24 13:16:18 +0000

    Country, Region, Most Played, Embed Changes,
    Added Country and Region to embed
    Added link to profile
    Added most played game and most played game play time fields
    Added another embed page for bans

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ import requests
 import asyncio
 from datetime import datetime
 from datetime import timedelta
+import pycountry as pc
+import pycountry_convert as pcc
 
 # Steam Ladder Base URL
 SL = 'https://steamladder.com/api/v1'
@@ -72,7 +74,12 @@ async def on_message(message):
             
             name = profile_data['response']['players'][0]['personaname']
             avatar = profile_data['response']['players'][0]['avatarfull']
-            country = profile_data['response']['players'][0]['loccountrycode']
+            country_code = profile_data['response']['players'][0]['loccountrycode']
+
+            country = pc.countries.get(alpha_2=f'{country_code}')
+            country = country.name
+            region = pcc.country_alpha2_to_continent_code(country_code)
+            region = pcc.convert_continent_code_to_continent_name(region)
 
             # GET PLAYER BANS
             bans_url = '{}/ISteamUser/GetPlayerBans/v1/?key={}&steamids={}'.format(SW, SWK, steamid)
@@ -112,6 +119,7 @@ async def on_message(message):
 
             if ladder_response.status_code == 200:
                 steam_join = ladder_data['steam_user']['steam_join_date']
+                steamladder_url = ladder_data['steam_user']['steamladder_url']
                 last_update = ladder_data['steam_stats']['last_update']
                 level = ladder_data['steam_stats']['level']
                 xp = ladder_data['steam_stats']['xp']
@@ -119,6 +127,8 @@ async def on_message(message):
                 badges = ladder_data['steam_stats']['badges']['total']
                 games = ladder_data['steam_stats']['games']['total_games']
                 playtime = ladder_data['steam_stats']['games']['total_playtime_min']
+                max_game = ladder_data['steam_stats']['games']['most_played'][0]['name']
+                max_playtime = ladder_data['steam_stats']['games']['most_played'][0]['playtime_min']
 
                 # XP
                 world_xp_rank = ladder_data['ladder_rank']['worldwide_xp']
@@ -143,26 +153,39 @@ async def on_message(message):
                 joined_years = round(joined_days/366)
 
                 playtime = round(playtime / 60)
+                max_playtime = round(max_playtime / 60)
                 
-            elif requests.status_code == 429:
-                print("Request rate limited: {}".format(requests.text))
-            elif requests.status_code == 401:
-                print("Not authorized: {}".format(requests.text))
-            elif requests.status_code == 404:
-                print("Not found: {}".format(requests.text))
+            elif ladder_response.status_code == 429:
+                print("Request rate limited: {}".format(ladder_response.text))
+            elif ladder_response.status_code == 401:
+                print("Not authorized: {}".format(ladder_response.text))
+            elif ladder_response.status_code == 404:
+                print("Not found: {}".format(ladder_response.text))
+                error = ladder_data['error']
+                await message.channel.send(f"An error ocurred: {error}")
+            else:
+                try:
+                    error = ladder_data['error']
+                except:
+                    await message.channel.send("A weird error happened, F")
+                await message.channel.send(f"An error ocurred: {error}")
 
             # WRITE DATA
             await message.channel.send(f'```{update}\nLast Data Update: {last_update}```')
-            embed = discord.Embed(title=f"{name} | Steam Profile", color=0x000000)
+
+            embed = discord.Embed(title=f"{name}", url=f"https://steamcommunity.com/profiles/{steamid}", description=f"{country}, {region}", color=0x000000)
+            embed.set_author(name=name, url=f"https://steamcommunity.com/profiles/{steamid}", icon_url=avatar)
             embed.set_thumbnail(url=avatar)
-            embed.add_field(name="Steam ID", value=f"{steamid}", inline=True)
-            embed.add_field(name="Country", value=f"{country}", inline=True)
+
+            embed.add_field(name="Steam ID", value=f"{steamid}", inline=False)
+
             embed.add_field(name="Steam Years", value=f"{joined_years}", inline=True)
             embed.add_field(name="Level", value=f"{level}", inline=True)
-            embed.add_field(name="Friends", value=f"{friends}", inline=True)
             embed.add_field(name="Badges", value=f"{badges}", inline=True)
             embed.add_field(name="Games", value=f"{games}", inline=True)
             embed.add_field(name="Playtime", value=f"{playtime}", inline=True)
+            embed.add_field(name="Friends", value=f"{friends}", inline=True)
+            embed.add_field(name="Most Played Game", value=f"{max_game} - {max_playtime}h", inline=False)
             embed.set_footer(text="By: UGH Dany")
             msg = await message.channel.send(embed=embed)
 
@@ -172,67 +195,53 @@ async def on_message(message):
             def check(reaction, user):
                 return user == message.author and str(reaction.emoji) == '▶'
             try:
-                reaction, user = await client.wait_for('reaction_add', check=check)
+                reaction, user = await client.wait_for('reaction_add', timeout=15.0, check=check)
             except asyncio.TimeoutError:
-                print('')
+                print()
             else:
-                embed2 = discord.Embed(title=f"{name} | Ladder Stats", color=0x000000)
-                embed2.add_field(name="Level World Rank", value=f"#{world_xp_rank}", inline=True)
-                embed2.add_field(name="Level Region Rank", value=f"#{region_xp_rank}", inline=True)
-                embed2.add_field(name="Level Country Rank", value=f"#{country_xp_rank}", inline=True)
+                embed2 = discord.Embed(color=0x000000, description=f"{country}, {region}")
+                embed2.set_author(name=f"{name} | SteamLadder Stats", url=steamladder_url, icon_url=avatar)
 
-                embed2.add_field(name="\u200B", value="\u200B", inline=False)
+                embed2.add_field(name="W | R | C", value="World | Region | Country", inline=False)
 
-                embed2.add_field(name="Playtime World Rank", value=f"#{world_playtime_rank}", inline=True)
-                embed2.add_field(name="Playtime Region Rank", value=f"#{region_playtime_rank}", inline=True)
-                embed2.add_field(name="Playtime Country Rank", value=f"#{country_playtime_rank}", inline=True)
+                embed2.add_field(name="Level W", value=f"#{world_xp_rank}", inline=True)
+                embed2.add_field(name="Playtime W", value=f"#{world_playtime_rank}", inline=True)
+                embed2.add_field(name="Games W", value=f"#{world_games_rank}", inline=True)
 
-                embed2.add_field(name="\u200B", value="\u200B", inline=False)
+                embed2.add_field(name="Level R", value=f"#{region_xp_rank}", inline=True)
+                embed2.add_field(name="Playtime R", value=f"#{region_playtime_rank}", inline=True)
+                embed2.add_field(name="Games R", value=f"#{region_games_rank}", inline=True)
 
-                embed2.add_field(name="Games World Rank", value=f"#{world_games_rank}", inline=True)
-                embed2.add_field(name="Games Region Rank", value=f"#{region_games_rank}", inline=True)
-                embed2.add_field(name="Games Country Rank", value=f"#{country_games_rank}", inline=True)
+                embed2.add_field(name="Level C", value=f"#{country_xp_rank}", inline=True)
+                embed2.add_field(name="Playtime C", value=f"#{country_playtime_rank}", inline=True)
+                embed2.add_field(name="Games C", value=f"#{country_games_rank}", inline=True)
                 
-                embed2.add_field(name="\u200B", value="\u200B", inline=False)
-
-                embed2.add_field(name="VAC Ban", value=f"{vac_ban}", inline=True)
-                embed2.add_field(name="Trade Ban", value=f"{trade_ban}", inline=True)
-                embed2.add_field(name="Community Ban", value=f"{community_ban}", inline=True)
                 embed2.set_footer(text="By: UGH Dany")
                 await msg.edit(embed=embed2)
                 await reaction.remove(user)
-                await msg.add_reaction('◀')
+                await reaction.remove(client.user)
+                await msg.add_reaction('▶')
                 user =  None
                 reaction = None
                 def check(reaction, user):
-                    return user == message.author and str(reaction.emoji) == '◀'
+                    return user == message.author and str(reaction.emoji) == '▶'
                 try:
-                    reaction, user = await client.wait_for('reaction_add', check=check)
+                    reaction, user = await client.wait_for('reaction_add', timeout=15.0, check=check)
                 except asyncio.TimeoutError:
-                    print('')
+                    print()
                 else:
-                    await msg.edit(embed=embed)
-                    await reaction.remove(user)
-            
-            #await msg.add_reaction('🔁')
+                    embed3 = discord.Embed(color=0x000000, description=f"{country}, {region}")
+                    embed3.set_author(name=f"{name} | Bans", url=steamladder_url, icon_url=avatar)
 
-            #user =  None
-            #reaction = None
-            #def check(reaction, user):
-            #    return user == message.author and str(reaction.emoji) == '🔁'
-            #try:
-            #    reaction, user = await client.wait_for('reaction_add', timeout=10.0, check=check)
-            #except asyncio.TimeoutError:
-            #    await channel.send('👎')
-            #else:
-            #    ladder_update_response = requests.post(ladder_url, headers={'Authorization': 'Token {}'.format(SLK)})
-            #    ladder_update_data = ladder_update_response.json()
-            #    if ladder_update_response.status_code == 200:
-            #        await channel.send('```Data Updated!```')
-            #    else:
-            #        await channel.send('```Can\'t Update Data Yet!```')
-
+                    embed3.add_field(name="VAC Ban", value=f"{vac_ban}", inline=True)
+                    embed3.add_field(name="Trade Ban", value=f"{trade_ban}", inline=True)
+                    embed3.add_field(name="Community Ban", value=f"{community_ban}", inline=True)
+                    
+                    embed3.set_footer(text="By: UGH Dany")
+                    await msg.edit(embed=embed3)
+                await reaction.remove(user)
+                await reaction.remove(client.user)
         else:
-            await message.channel.send('Steam Custom URL Not Found!\n||(If you\'re sure this is an error, DM UGH Dany)||')
+            await message.channel.send('Steam Custom URL Not Found!\n||(If you\'re sure this is an error, DM @UGH Dany)||')
 
 client.run(token)
